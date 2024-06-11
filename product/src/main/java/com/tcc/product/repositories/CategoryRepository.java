2da2a76387cbd2202da1a0b81cb4c71047500cda
mateusbrandao <mateus.brandaoteixeira@gmail.com>
2024-06-11 14:34:30 -0300

    remove product

--- product/src/main/java/com/tcc/product/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcc.product.repositories;
-
-import com.tcc.product.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- product/src/main/java/com/tcc/product/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tcc.product.repositories;
-
-import com.tcc.product.models.Category;
-import com.tcc.product.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
